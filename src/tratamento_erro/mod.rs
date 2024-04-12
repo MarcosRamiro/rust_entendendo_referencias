e878ec9c09dd596b4db7e6c1c236be84a83274fb
Marcos Ramiro <mrsantos2006@hotmail.com>
2024-04-11 21:57:24 -0300

    feature: tratamento de erro na mão

--- src/tratamento_erro/mod.rs
+++ src/tratamento_erro/mod.rs
@@ -1,24 +1,38 @@
 use std::error::Error;
 
 #[derive(Debug)]
-pub enum Erro {
+pub enum ErroPersonalizado {
     mensagem(String),
     NaoLocalizado,
+    OutroErro(Box<dyn Error>),
 }
 
-impl Error for Erro {}
+impl Error for ErroPersonalizado {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            ErroPersonalizado::OutroErro(causa) => Some(causa.as_ref()),
+            _ => None,
+        }
+    }
+}
 
-impl std::fmt::Display for Erro {
+impl std::fmt::Display for ErroPersonalizado {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Erro::mensagem(msg) => write!(f, "{}", msg),
+            ErroPersonalizado::mensagem(msg) => write!(f, "{}", msg),
             _ => write!(f, "Erro desconhecido"),
         }
     }
 }
 
 pub fn como_tratar_erros() -> Result<(), Box<dyn Error>> {
-    let valor = outra_funcao_que_da_erro()?;
+    let resultado = outra_funcao_que_da_erro();
+    if let Err(erro) = resultado {
+        if let Some(erro_source) = erro.source() {
+            println!("Causa: {}", erro_source);
+        }
+        return Err(erro);
+    }
     Ok(())
 }
 
@@ -26,12 +40,14 @@ fn outra_funcao_que_da_erro() -> MyErro<()> {
     if rand::random() {
         // generates a boolean
         return Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::InvalidData,
+            std::io::ErrorKind::Other,
             "Erro de exemplo",
         )));
     }
 
-    Err(Box::new(Erro::mensagem("Aqui deu ruim".to_string())))
+    Err(Box::new(ErroPersonalizado::OutroErro(
+        Box::new(ErroPersonalizado::mensagem("Meu erro personalizado.".to_string())),
+    )))
 }
 
 pub type MyErro<T> = Result<T, Box<dyn Error>>;
